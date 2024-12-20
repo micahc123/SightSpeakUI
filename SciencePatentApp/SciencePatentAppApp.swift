9f99907c5fa7e254e0da905a57a8b5375c336d33
Micah C <106202832+micahc123@users.noreply.github.com>
2024-12-20 14:04:55 +0800

    Initial Commit

---
+++ SciencePatentApp/SciencePatentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SciencePatentAppApp.swift
+//  SciencePatentApp
+//
+//  Created by Micah Chen on 20/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SciencePatentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SciencePatentAppTests/SciencePatentAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SciencePatentAppTests.swift
+//  SciencePatentAppTests
+//
+//  Created by Micah Chen on 20/12/2024.
+//
+
+import Testing
+@testable import SciencePatentApp
+
+struct SciencePatentAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
